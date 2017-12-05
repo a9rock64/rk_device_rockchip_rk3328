f8c5943af0d2e0ae17bfc265ed3593a16353bcc5
Steven Chen <cw@rock-chips.com>
2017-12-05 16:49:56 +0800

    GTVS: Configured for rk3328
    Change-Id: I4bd4fe9a630b31660b8a4fc983824111914ad083
    Signed-off-by: Steven Chen <cw@rock-chips.com>

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ PRODUCT_COPY_FILES += \
     device/rockchip/rk3328/package_performance.xml:$(TARGET_COPY_OUT_OEM)/etc/package_performance.xml \
     device/rockchip/$(TARGET_BOARD_PLATFORM)/media_profiles_default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     device/rockchip/rk3328/ff1b0030_pwm.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ff1b0030_pwm.kl \
-    device/rockchip/rk3328/ff1b0030_pwm.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/ff1b0030_pwm.idc
+    device/rockchip/rk3328/ff1b0030_pwm.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/ff1b0030_pwm.idc \
+    device/rockchip/rk3328/virtual-remote.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/virtual-remote.idc
 
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
@@ -111,3 +112,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_flushrate=0.4 \
     ro.hwui.texture_cache_size=48.0 \
 
+# GTVS add the Client ID (provided by Google)
+PRODUCT_PROPERTY_OVERRIDES += \
+ ro.com.google.clientidbase=android-acme
