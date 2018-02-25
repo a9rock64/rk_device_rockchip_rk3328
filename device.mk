cf281412761a0f19c2609d6f4a9934a8f6bb1ab4
Huacong Yang <will.yang@rock-chips.com>
2018-02-25 10:36:52 +0800

    add vendor seccomp policy files for media components
    Change-Id: I5b698b1068fb826c1dd6c68acd6808ee5ecd596e

--- device.mk
+++ device.mk
@@ -113,3 +113,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # GTVS add the Client ID (provided by Google)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-acme
+
+# Vendor seccomp policy files for media components:
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
