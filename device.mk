833a1a72b9cedf10615ac3cddde450a45ed96911
Huacong Yang <will.yang@rock-chips.com>
2018-02-27 16:40:41 +0800

    add copy libplayreadydrmplugin
    Change-Id: I9a6be9a3b79cd1c9adb441f11645f5c82fcd83c5

--- device.mk
+++ device.mk
@@ -118,3 +118,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+PRODUCT_PACKAGES += \
+    libplayreadydrmplugin
+
