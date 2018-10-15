eaaaaf165a752c5ba7800f783f2f7b3fe8b62723
huangjc <huangjc@rock-chips.com>
2018-10-15 17:59:58 +0800

    ATV:temp:set androidboot.verifiedbootstate=orange for boot
    Change-Id: I06d92b075059c827a8e661e7bb9ad8fcc4e75cc2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,7 @@ PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 else
 #Config the cmdline for boot or recpvery
-BOARD_KERNEL_CMDLINE := console=ttyFIQ0 androidboot.baseband=N/A androidboot.selinux=enforcing androidboot.wificountrycode=US androidboot.veritymode=enforcing androidboot.hardware=rk30board androidboot.console=ttyFIQ0 firmware_class.path=/vendor/etc/firmware init=/init rootwait ro init=/init root=PARTUUID=af01642c-9b84-11e8-9b2a-234eb5e198a0
+BOARD_KERNEL_CMDLINE := console=ttyFIQ0 androidboot.baseband=N/A androidboot.selinux=enforcing androidboot.wificountrycode=US androidboot.veritymode=enforcing androidboot.hardware=rk30board androidboot.verifiedbootstate=orange androidboot.console=ttyFIQ0 firmware_class.path=/vendor/etc/firmware init=/init rootwait ro init=/init root=PARTUUID=af01642c-9b84-11e8-9b2a-234eb5e198a0
 endif
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 
